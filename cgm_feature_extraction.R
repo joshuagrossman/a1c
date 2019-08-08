e3ec7fb5b597f1e949b49ac22bb18dbd240c1bb2
Josh Grossman <joshuag200@gmail.com>
2019-08-07 20:34:46 -0700

    finalized cgm feature extraction

--- cgm_feature_extraction.R
+++ cgm_feature_extraction.R
@@ -2,10 +2,12 @@
 # DONE Max number of records for each day
 # DONE Low BG limit
 # DONE High BG limit
-# Minimum BG cutoff (check data)
-# Maximum BG cutoff (check data)
-# ranges, sleep 12 AM - 6 AM, wake 6 AM - 12 AM, 24 hours
+# DONE ranges, sleep 12 AM - 6 AM, wake 6 AM - 12 AM, 24 hours
 # DONE 70-80% data required (make sure satisfied if 15 min recordings)
+
+
+# Minimum BG cutoff (check data?)
+# Maximum BG cutoff (check data?)
 
 
 # Features
@@ -14,22 +16,23 @@
 # DONE Average BG at each day interval
 # DONE TIR in each day interval 70-180, 70-140
 # DONE SD at each interval
-# CV at each interval
 # DONE Time very high >250
 # DONE Time high >180 <250
 # DONE Time low >54 <70
 # DONE Time very low <54
-# Area under the curve - average at each day interval?
+# DONE Area under the curve - average at each day interval, normalized by day
+
 # Number of hypo / hyperglycemic events per day
+# CV at each interval
 
-# How to filter missing data? By day interval? Or by day? -- Full Day in the past
+# DONE How to filter missing data? By day interval? Or by day? -- Full Day in the past
 
-# How to average/sd results? Over all measurements (unweighted) or by day? -- Average by day
+# DONE How to average/sd results? Over all measurements (unweighted) or by day? -- Average by day
 
-# How to calculate AUC for missing data? Weight it by the data points you
+# DONE How to calculate AUC for missing data? Weight it by the data points you
 # actually have
 
-# How to report AUC feature? Average AUC?  -- Normalize it by day
+# DONE How to report AUC feature? Average AUC?  -- Normalize it by day
 
 # Brainstorming additional features worth extracting
 
@@ -57,15 +60,7 @@
 # scatterplot for both models of actual versus predicted, color the points by the
 # value of the demographic of interest
 
-################################## LIBRARIES ###################################
-
-library(tidyverse)
-library(fs)
-library(rlang)
-library(lubridate)
-library(furrr)
-
-plan(multisession(workers = availableCores()))
+source("/lib/load.R")
 
 ################################## GLOBALS #####################################
 
@@ -87,98 +82,40 @@ RANGE_NAMES <- c("very_low",
                  "high", 
                  "very_high")
 
-MIN_BG_CUTOFF <- 40
-MAX_BG_CUTOFF <- 400
-
 MIN_DATA_REQUIRED <- 0.7
 
-CGM_HEADERS <- c("glucose", "datetime")
+################################## CALCULATE CGM FEATURES ######################
 
-################################## LOAD DATA ###################################
-
-load_large_file <- function(filepath) {
-  # Input: Filepath to a CSV containing CGM data for more than one patient. The
-  # CSV must contain patient id info, glucose values, and timestamps.
+make_cgm_feature_df <- function(cgm_data_with_id) {
+  # Input: List containing CGM data for a particular patient and the patient's id
   #
-  # Output: A list of lists, with each list containing the patient_id and all
-  # associated CGM data.
+  # Output: One-row dataframe containing summary features for the patient's CGM data.
   
-  if (path_ext(filepath) != "csv") {
-    abort("CGM datafile must be a CSV.")
-  }
+  id <- as.character(cgm_data_with_id$id)
+  cgm_data <- cgm_data_with_id$data
   
-  df <- read_csv(filepath)
+  sufficient_cgm_data <- filter_insufficient_data(cgm_data)
   
-  headers_with_id <- c("id", CGM_HEADERS)
-  
-  if (! all(colnames(df) %in% headers_with_id)) {
-    abort(str_c("CGM datafile must contain ", headers_with_id))
-  }
-  
-  group_by(df, id) %>% 
-    group_map(~ list(id = .y, data = .x))
+  calculate_all_bg_stats(sufficient_cgm_data)
 }
 
-# load_individual_file <- function(filepath) {
-#   
-#   if (path_ext(filepath) != "csv") {
-#     abort("CGM datafile must be a CSV.")
-#   }
-#   
-#   df <- read_csv(filepath)
-#   
-#   if (! all(colnames(df) %in% CGM_HEADERS)) {
-#     abort(str_c("CGM datafile must contain ", CGM_HEADERS))
-#   }
-#   
-#   id <- path_ext_remove(path_file(filepath))
-#   
-#   list(id = id, data = df)
-# }
-# 
-# load_dir <- function(dirpath) {
-#   
-#   if (! is_dir(dirpath)) {
-#     abort("CGM data folder must be a directory.")
-#   }
-#   
-#   dir_map(dirpath, load_individual_file)
-# }
-
-################################## PIPELINE ####################################
-
-make_feature_df <- function(filepath) {
-  # Input: Filepath to a CSV containing CGM data for all patients.
-  #
-  # Output: Dataframe containing summary features for each patient's CGM data. 
-  
-  patient_data_with_ids <- load_large_file(filepath)
-  
-  ids <- map_chr(patient_data_with_ids, ~ as.character(.x$id))
-  patient_data <- map(patient_data_with_ids, ~ .x$data)
-  
-  augmented_patient_data <- future_map(patient_data, count_records_per_interval)
-  
-  future_map(augmented_patient_data, calculate_all_bg_stats) %>% 
-    reduce(rbind) %>%  
-    as_tibble() %>% 
-    mutate(id = ids)
-}
-
-################################## FILTER INSUFFICIENT DATA ####################
+############# FILTER TOO OLD, TOO RECENT, AND INSUFFICIENT DATA ################
 
 Mode <- function(x) {
+  
   # R doesn't have a base mode function...
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
 
-calculate_minutes_per_record <- function(df, n = 1000) {
+calculate_minutes_per_record <- function(bg_df, n = 1000) {
   # Estimates the amount of time between CGM readings for a given patient's
-  # CGM data. Assumes that all CGM data for a particular patient will have one
+  # CGM data. 
+  #
+  # Assumes that all CGM data for a particular patient will have one
   # common time between recordings. 
   
-  datetimes <- df$datetime
+  datetimes <- bg_df$datetime
   
   n_rows <- length(datetimes)
   
@@ -196,54 +133,48 @@ calculate_minutes_per_record <- function(df, n = 1000) {
     Mode()
 }
   
-make_intervals <- function(df) {
-  # Converts raw timestamps to date and hour equivalents, and indicates 
+make_intervals <- function(bg_df) {
+  # Extracts date and hour from timestamps, and indicates 
   # measurements that occur during nighttime. 
   
-  df %>% 
+  bg_df %>% 
     mutate(cgm_day = lubridate::date(datetime),
            cgm_hour = lubridate::hour(datetime),
            nighttime = cgm_hour < DAY_START)
 }
 
-calculate_records_per_hour <- function(df) {
+calculate_records_per_hour <- function(minutes_per_record) {
   # Determine max possible CGM recordings per hour.
   
-  mode_time_between_records <- calculate_minutes_per_record(df)
-  
-  if (! mode_time_between_records %in% MINUTES_BETWEEN_RECORDS) {
-    print(df)
+  if (! minutes_per_record %in% MINUTES_BETWEEN_RECORDS) {
+    print(minutes_per_record)
     abort("CGM data not recorded in 5, 10, 15, or 20 minute intervals.")
   }
   
-  # calculates how many times more you are recording per hour compared to 
-  # 20 minute recording intervals (3 times per hour)
-  
-  cgm_record_multiplier <- 1
-  
-  if (mode_time_between_records == 5) {
-    cgm_record_multiplier <- 4
-  }
-  
-  if (mode_time_between_records == 10) {
-    cgm_record_multiplier <- 2
-  }
-  
-  if (mode_time_between_records == 15) {
-    cgm_record_multiplier <- 4/3
-  }
-  
-  3 * cgm_record_multiplier
+  60 / minutes_per_record
 }
 
-count_records_per_interval <- function(df) {
+filter_data_by_date <- function(bg_df, 
+                                start_date_chr = "1900-01-01",
+                                end_date_chr = "2100-12-31") {
+  # Removed CGM data for invalid dates.
+  
+  start_date <- as.Date(start_date_chr)
+  end_date <- as.Date(end_date_chr)
+  
+  bg_df <-
+    filter(bg_df, between(datetime, start_date, end_date))
+}
+                                
+filter_insufficient_data <- function(bg_df) {
   # Counts total possible CGM records for a given day and calculates the fraction
   # of records actually recorded. Removes data from days with less than 
   # MIN_DATA_REQUIRED fraction of records present.
   
-  cgm_records_per_hour <- calculate_records_per_hour(df)
+  cgm_records_per_hour <- calculate_minutes_per_record(bg_df) %>% 
+                            calculate_records_per_hour()
   
-  interval_df <- make_intervals(df)
+  interval_df <- make_intervals(bg_df)
   
   exclude_df <-
     interval_df %>%
@@ -255,45 +186,26 @@ count_records_per_interval <- function(df) {
     select(cgm_day)
   
   anti_join(interval_df, exclude_df, by = c("cgm_day"))
-    
-  # DEPRECATED: this code removed data by day/night interval
-  #.
-  # TODO: Make this more robust with DAY_START and DAY_END instead of using
-  # midnight default as day end
-  # 
-  # daytime_record_multiplier <- ((24 - DAY_START) / DAY_START) - 1
-  #
-  # exclude_df <-
-  #   interval_df %>%
-  #   group_by(cgm_day, nighttime) %>%
-  #   summarize(n = n()) %>%
-  #   ungroup() %>%
-  #   mutate(prop = n / (cgm_records_per_hour *
-  #                       (DAY_START *
-  #                         (1 + (! nighttime) * daytime_record_multiplier)))) %>%
-  #   filter(prop < MIN_DATA_REQUIRED) %>%
-  #   select(cgm_day, nighttime)
-  # 
-  # anti_join(interval_df, exclude_df, by = c("cgm_day", "nighttime"))
 }
 
-################################## CALCULATE BG STATS ##########################
+################################## SIMPLE BG STATS #############################
 
-calculate_cgm_days <- function(df) {
+calculate_cgm_days <- function(bg_df) {
   # Calculates the total number of days for which CGM data is available
   
-  c(cgm_days = length(unique(df$cgm_day)))
+  c(cgm_days = length(unique(bg_df$cgm_day)))
 }
 
-calculate_simple_bg_stat <- function(df, f, stat_name) {
-  # Input: df: dataframe with CGM data, f: function to perform on glucose values,
+calculate_simple_bg_stat <- function(bg_df, f, stat_name) {
+  # bg_df: dataframe with CGM data, 
+  # f: function to perform on glucose values,
   # stat_name: a string describing f
   #
-  # Output: a named vector of the values calculated by applying f during the
-  # full day, nighttime, and daytime.
+  # Output: a named vector of the values calculated by applying f to glucose
+  # values during the full day, nighttime, and daytime.
   
-  bg <- df$glucose
-  nighttime <- df$nighttime
+  bg <- bg_df$glucose
+  nighttime <- bg_df$nighttime
   bg_nighttime <- bg[nighttime]
   bg_daytime <- bg[! nighttime]
   
@@ -306,10 +218,12 @@ calculate_simple_bg_stat <- function(df, f, stat_name) {
   bg_stat
 }
 
-make_bg_booleans <- function(df) {
+################################## TIME IN RANGE ###############################
+
+make_bg_booleans <- function(bg_df) {
   # Determines which range a particular CGM recording falls in.
   
-  df %>% 
+  bg_df %>% 
     mutate(very_low = glucose < VERY_LOW_BG_LIMIT,
            low = glucose < LOW_BG_LIMIT,
            in_target_range = between(glucose, LOW_BG_LIMIT, HIGH_BG_LIMIT),
@@ -326,16 +240,20 @@ na_sd <- function(v) {
   sd(v, na.rm = T)
 }
 
-calculate_percent_in_given_range <- function(df, range_name) {
+calculate_percent_in_given_range <- function(bg_df, range_name) {
   # Calculates the percent of valid CGM readings that fall in the supplied
   # range_name (e.g. "very_high")
   #
-  # Assumes that range_name is also a column in df (created by make_bg_booleans)
+  # Assumes that range_name is also a column in bg_df (created by make_bg_booleans)
   
-  in_range <- pull(df, range_name)
+  if (! range_name %in% colnames(bg_df)) {
+    abort(str_c(range_name, " column not contained in argument `bg_df` of `calculate_percent_in_given_range`."))
+  }
+  
+  in_range <- pull(bg_df, range_name)
   full_day_percent_in_range <- na_mean(in_range)
   
-  nighttime <- df$nighttime
+  nighttime <- bg_df$nighttime
   in_range_nighttime <- in_range[nighttime]
   in_range_daytime <- in_range[! nighttime]
     
@@ -355,43 +273,77 @@ calculate_percent_in_given_range <- function(df, range_name) {
   percent_in_range_features
 }
 
-calculate_auc <- function(df) {
+################################## AUC #########################################
+
+calculate_auc <- function(bg_df) {
   # Calculates the AUC for a given day of CGM data, and scales the AUC by the
   # number of valid recordings for that day
   
-  time_diffs <- df$datetime %>% 
+  time_diffs <- bg_df$datetime %>% 
                   diff() %>% 
                   time_length(unit = "minutes") %>% 
                   abs() %>% 
                   round()
   
-  n_records <- calculate_cgm_days(df)
+  minutes_per_record <- calculate_minutes_per_record(bg_df)
   
-  glucose <- df$glucose
+  max_records_per_day <- 24 * calculate_records_per_hour(minutes_per_record)
   
-  # TODO: if time_diff is the same as normal recording interval, calculate, else ignore
+  glucose <- bg_df$glucose
+  
+  # if time_diff is the same as normal recording interval, calculate auc, else 0
   aucs <- pmap(list(head(glucose, -1),
                     glucose[-1],
                     time_diffs),
-       ~ ..3 * (..1 + ..2) / 2)
+       ~ if (..3 == minutes_per_record) {..3 * (..1 + ..2) / 2} else 0)
   
-  sum(unlist(aucs)) / n_records
+  aucs <- unlist(aucs)
+  
+  n_valid_aucs <- sum(aucs != 0)
+  
+  # if 288 possible recordings in a day, 287 possible aucs can be recorded
+  sum(aucs) * ((max_records_per_day - 1)  / n_valid_aucs)
 }
 
-calculate_average_auc <- function(df) {
+calculate_average_auc <- function(bg_df, range_name = NULL) {
+  
+  # if (! is.null(range_name)) {
+  #   
+  #   if (! range_name %in% colnames(bg_df)) {
+  #     abort(str_c(range_name, " column not contained in argument `bg_df` of `calculate_average_auc`."))
+  #   }
+  #   
+  #   in_range <- pull(bg_df, range_name)
+  #   
+  #   minutes_per_record <- calculate_minutes_per_record(bg_df)
+  #   
+  #   records_per_half_hour <- ceiling(60 / minutes_per_record)
+  #   
+  #   # collapse in_range 0 1 1 0 0 0 1 1 1 0 0 0 1 1 1 1 into 0 2 000 3 000 4
+  #   # if the numbers are >= records_per_half_hour, expand into corresponding number of 1s
+  #   # else expand into corresponding numbers of 0s
+  #
+  #   in_range_at_least_half_hour <- 
+  #   
+  #   bg_df <-
+  #     filter(bg_df, in_range_at_least_half_hour)
+  # }
+  
   average_auc <- 
-    df %>% 
+    bg_df %>% 
     group_by(cgm_day) %>% 
     group_map(~ calculate_auc(.x)) %>% 
     unlist() %>% 
     mean()
-    
-  c(average_auc = average_auc)
+  
+  names(average_auc) <- str_c("auc_", range_name)
+  
+  average_auc
 }
 
-calculate_all_bg_stats <- function(df) {
+calculate_all_bg_stats <- function(bg_df) {
   
-  bg_df <- make_bg_booleans(df)
+  bg_df <- make_bg_booleans(bg_df)
   
   features <- c(calculate_cgm_days(bg_df),
                 calculate_simple_bg_stat(bg_df, na_mean, "mean"),
@@ -401,5 +353,61 @@ calculate_all_bg_stats <- function(df) {
   in_range_stats <- map(RANGE_NAMES, 
                         ~ calculate_percent_in_given_range(bg_df, .))
   
-  features <- c(features, unlist(in_range_stats))
+  # auc_stats <- map(RANGE_NAMES, 
+  #                       ~ calculate_average_auc(bg_df, .))
+  
+  features <- c(features, unlist(in_range_stats))#, unlist(auc_stats))
 }
+
+
+
+################################## DEPRECATED ##################################
+
+# load_individual_file <- function(filepath) {
+#   
+#   if (path_ext(filepath) != "csv") {
+#     abort("CGM datafile must be a CSV.")
+#   }
+#   
+#   bg_df <- read_csv(filepath)
+#   
+#   if (! all(colnames(bg_df) %in% CGM_HEADERS)) {
+#     abort(str_c("CGM datafile must contain ", CGM_HEADERS))
+#   }
+#   
+#   id <- path_ext_remove(path_file(filepath))
+#   
+#   list(id = id, data = bg_df)
+# }
+# 
+# load_dir <- function(dirpath) {
+#   
+#   if (! is_dir(dirpath)) {
+#     abort("CGM data folder must be a directory.")
+#   }
+#   
+#   dir_map(dirpath, load_individual_file)
+# }
+
+
+
+
+# DEPRECATED: this code removed data by day/night interval
+#.
+# TODO: Make this more robust with DAY_START and DAY_END instead of using
+# midnight default as day end
+# 
+# daytime_record_multiplier <- ((24 - DAY_START) / DAY_START) - 1
+#
+# exclude_df <-
+#   interval_df %>%
+#   group_by(cgm_day, nighttime) %>%
+#   summarize(n = n()) %>%
+#   ungroup() %>%
+#   mutate(prop = n / (cgm_records_per_hour *
+#                       (DAY_START *
+#                         (1 + (! nighttime) * daytime_record_multiplier)))) %>%
+#   filter(prop < MIN_DATA_REQUIRED) %>%
+#   select(cgm_day, nighttime)
+# 
+# anti_join(interval_df, exclude_df, by = c("cgm_day", "nighttime"))
--- load.R
+++ load.R
@@ -2,82 +2,37 @@
 
 library(tidyverse)
 library(fs)
+library(rlang)
 library(lubridate)
+library(furrr)
 
-################################## READING FILES ###############################
+plan(multisession(workers = availableCores()))
 
-is_csv <- function(file) {
-  first_line <- read_lines(file, n_max = 1)
-  n_chars <- str_length(first_line)
-  n_commas <- str_count(first_line, ",")
-  n_pipes <- str_count(first_line, "\\|")
+################################## GLOBALS #####################################
+
+CGM_HEADERS <- c("id", "glucose", "datetime")
+
+A1C_HEADERS <- c("id", "a1c", "date")
+
+################################## LOAD DATA ###################################
+
+load_file <- function(filepath, required_headers) {
+  # Input: Filepath to a CSV containing data for more than one patient. The
+  # CSV must contain required_headers as columns.
+  #
+  # Output: A list of lists, with each list containing the patient_id and all
+  # associated data as a df.
   
-  return((n_commas/n_chars) > (n_pipes/n_chars))
-}
-
-parse_file <- function(filename) {
-  
-  if(is_dir(filename)) {
-    warning(str_c("Can't parse a directory: ", filename))
-    return(NULL)
+  if (path_ext(filepath) != "csv") {
+    abort("Datafile at `filepath` must be a CSV.")
   }
   
-  if (is_csv(filename)) {
-    return(read_csv(filename, col_types = cols(.default = "c")))
+  df <- read_csv(filepath)
+  
+  if (! all(colnames(df) %in% required_headers)) {
+    abort(str_c("Datafile at `filepath` must contain ", required_headers))
   }
   
-  read_delim(filename, "|", col_types = cols(.default = "c"))
-}
-
-make_dfs <- function(path) {
-  filenames <- dir_ls(path)
-  dfs <- map(filenames, parse_file)
-  base_filenames <- str_replace(filenames, ".*/(.+)\\.[:alpha:]{3}", "\\1")
-  names(dfs) <- base_filenames
-  dfs
-}
-
-################################## MUTATING CSVS ###############################
-
-convert_to_date <- function(days_since_enrollment, study_start_date) {
-  mdy(study_start_date) + days(days_since_enrollment)
-}
-
-convert_to_datetime <- function(days_since_enrollment, device_time, study_start_date) {
-  mdy_hms(str_c(study_start_date, " ", device_time)) + days(days_since_enrollment)
-}
-
-inches_to_cm <- function(inches) {
-  2.54 * as.double(inches)
-}
-
-lbs_to_kg <- function(lbs) {
-  0.4536 * as.double(lbs)
-}
-
-################################## WRITING CSVS ################################
-
-write_main_csvs <- function(cleaned_dfs, cleaned_data_path) {
-  map2(cleaned_dfs,
-       names(cleaned_dfs),
-       ~ write_csv(.x, str_c(cleaned_data_path, "/", .y, ".csv")))
-}
-
-write_cgm_csv_for_patient <- function(df, cleaned_data_path) {
-  patient_id <- df$id[1]
-  df_no_id <- select(df, -id)
-  write_csv(df_no_id, str_c(cleaned_data_path,
-                      "/cgm_by_patient/",
-                      patient_id, 
-                      ".csv"))
-}
-
-split_cgm_into_patients <- function(cleaned_data_path) {
-  cgm <- 
-    read_csv(str_c(cleaned_data_path, "/cgm.csv")) %>% 
-    group_by(id)
-  
-  group_walk(cgm, 
-             ~ write_cgm_csv_for_patient(., cleaned_data_path), 
-             keep = T)
+  group_by(df, id) %>% 
+    group_map(~ list(id = .y, data = .x))
 }
