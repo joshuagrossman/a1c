5d5b2d2db6182fe6fdf5a1343b3952caa554bf28
Josh Grossman <jdgg@soal-3.stanford.edu>
2019-08-14 16:15:10 -0700

    getting cluster in sync with local

---
+++ cleaning_data/CMetforminDataset.R
@@ -0,0 +1,108 @@
+################################## LIBRARIES ###################################
+
+source("lib/load.R")
+
+################################## GLOBALS #####################################
+
+DATA_PATH <- "data/CMetforminDataset/Data Tables"
+CLEANED_DATA_PATH <- "data/CMetforminDataset/clean"
+DATA_NAME <- "CMetforminDataset"
+STUDY_START_DATE <- "07-02-2014"
+
+################################# READ IN DATA #################################
+
+dfs <- make_dfs(DATA_PATH)
+
+#View(dfs)
+
+cleaned_dfs <- list()
+
+################################# CGM DATA #####################################
+
+cleaned_dfs$cgm <- 
+  dfs$CDataCGM %>% 
+  filter(!is.na(Glucose)) %>% 
+  mutate(datetime = convert_to_datetime(DeviceDaysFromEnroll, 
+                                        DeviceTm, 
+                                        STUDY_START_DATE)) %>% 
+  select(id = PtID, 
+         datetime, 
+         glucose = Glucose)
+
+################################# HBA1C DATA ###################################
+
+cleaned_dfs$a1c <- 
+  dfs$CLabHbA1c %>% 
+  mutate(date = convert_to_date(TestDaysFromEnroll,
+                                STUDY_START_DATE)) %>% 
+  select(id = PtID, 
+         date, 
+         a1c = HbA1c)
+
+# Impossible to determine when these a1c measurements occurred, seems like they
+# are before the study start date
+# SampleResults_cleaned <- 
+#   dfs$SampleResults %>% 
+#   filter(ResultName == "HbA1c") %>% 
+#   select(id, a1c = Value)
+
+################################ STATIC MEASUREMENTS ###########################
+
+CRandomization_cleaned <-
+  dfs$CRandomization %>% 
+  select(id = PtID, 
+         weight = Weight, 
+         height = Height)
+
+CScreening_cleaned <-
+  dfs$CScreening %>% 
+  mutate(diag_date = str_replace(T1DDiagYrMon, 
+                                 "(\\d{4})(\\d{2})", 
+                                 "\\2-01-\\1") %>% 
+                     mdy(),
+         estimated_dob = diag_date - years(DiagT1DAge),
+         age = interval(estimated_dob, 
+                                  mdy(STUDY_START_DATE)) / years(1),
+         years_since_diag = age - as.double(DiagT1DAge)) %>% 
+  select(id = PtID, 
+         gender = Gender, 
+         ethnicity = Ethnicity, 
+         race = Race, 
+         age,
+         years_since_diag)
+
+CTrtGroupUnmasked_cleaned <-
+  dfs$CTrtGroupUnmasked %>% 
+  select(id = PtID, 
+         metformin = TrtGroup)
+
+cgm_name <-
+  dfs$CDataCGM %>% 
+  group_by(PtID) %>% 
+  select(id = PtID, 
+         cgm_name = CCGMDeviceType) %>% 
+  summarize(cgm_name = unique(cgm_name))
+
+cleaned_dfs$measurements <- 
+  full_join(CRandomization_cleaned, CScreening_cleaned, by = "id") %>% 
+  full_join(CTrtGroupUnmasked_cleaned, by = "id") %>% 
+  full_join(cgm_name, by = "id") %>% 
+  mutate(datafile = str_c(.$id, ".csv"),
+         study_start_date = STUDY_START_DATE,
+         dataset = DATA_NAME)
+
+################################ DATA CHECKS ###################################
+
+only_unique_patients <- nrow(cleaned_dfs$measurements) == 
+  length(unique(cleaned_dfs$measurements$id))
+  
+if (!only_unique_patients) {
+  warning("Non-unique patient data.")
+}
+
+################################ WRITE CSVS TO FILE ############################
+
+write_main_csvs(cleaned_dfs, CLEANED_DATA_PATH)
+
+split_cgm_into_patients(CLEANED_DATA_PATH)
+
---
+++ feature_extraction/cgm_feature_extraction.R
@@ -0,0 +1,453 @@
+# Constants
+# DONE Max number of records for each day
+# DONE Low BG limit
+# DONE High BG limit
+# DONE ranges, sleep 12 AM - 6 AM, wake 6 AM - 12 AM, 24 hours
+# DONE 70-80% data required (make sure satisfied if 15 min recordings)
+
+
+# Minimum BG cutoff (check data?)
+# Maximum BG cutoff (check data?)
+
+
+# Features
+# DONE Count of valid records at each day interval
+# DONE Fraction of max records valid at each day interval
+# DONE Average BG at each day interval
+# DONE TIR in each day interval 70-180, 70-140
+# DONE SD at each interval
+# DONE Time very high >250
+# DONE Time high >180 <250
+# DONE Time low >54 <70
+# DONE Time very low <54
+
+# Mean BG when low / high
+# SD BG when low / high
+
+# Number of hypo / hyperglycemic events per day
+# CV at each interval
+
+# DONE How to filter missing data? By day interval? Or by day? -- Full Day in the past
+
+# DONE How to average/sd results? Over all measurements (unweighted) or by day? -- Average by day
+
+# Brainstorming additional features worth extracting
+
+# Rate of change - only include times of 1-2 hours of increase
+
+# Rate of change of BG across hours, versus half hours, versus 15 minutes... as 
+# a way to define the complexity of the curve
+
+# What is the 90th percentile of the highs for a particular interval
+
+# Percentile statistics as opposed to average (don't use max/min)
+
+# Google definition of fractal dimensions for curves
+
+# Keeping a list of how to analyze CGM data different ways - averaging over days versys averaging over all points
+
+# Sensitivity analysis - calculate all of these metrics for a particular person
+# removing 1, 2, 3, ... data points at random, quantify bias of missing data
+
+# Random forest, LASSO, GBM, and XGBoost, compare against eA1c
+
+# 10-fold CV to tune parameters with grid search, good metric to report is R^2, 
+# scatterplot for both models of actual versus predicted, color the points by the
+# value of the demographic of interest
+
+source("lib/load.R")
+
+################################## GLOBALS #####################################
+
+MINUTES_BETWEEN_RECORDS <- c(5, 10, 15, 20)
+
+DAY_START <- 6
+# DAY_END assumed as midnight.
+
+LOW_BG_LIMIT <- 70
+VERY_LOW_BG_LIMIT <- 54
+CONSERVATIVE_HIGH_BG_LIMIT <- 140
+HIGH_BG_LIMIT <- 180
+VERY_HIGH_BG_LIMIT <- 250
+
+RANGE_NAMES <- c("very_low", 
+                 "low", 
+                 "in_target_range", 
+                 "in_conservative_target_range", 
+                 "high", 
+                 "very_high")
+
+MIN_DATA_REQUIRED <- 0.7
+
+# data points required to calculate summary stats
+MIN_RECORDINGS_REQUIRED <- 10
+
+################################## CALCULATE CGM FEATURES ######################
+
+make_cgm_feature_df <- function(cgm_data_with_id, 
+                                id_name = "id", 
+                                data_name = "data") {
+  # Input: List containing CGM data for a particular patient and the patient's id, as generate by load_file()
+  #
+  # Output: One-row dataframe containing summary features for the patient's CGM data.
+  
+  id <- as.character(cgm_data_with_id[[id_name]])
+  cgm_data <- cgm_data_with_id[[data_name]] %>% 
+    arrange(datetime)
+  
+  if (nrow(cgm_data) < MIN_RECORDINGS_REQUIRED) {
+    warning(str_c("Less than ", MIN_RECORDINGS_REQUIRED, 
+                  "data points. Returning NULL."))
+    return(NULL)
+  }
+  
+  sufficient_cgm_data <- filter_insufficient_data(cgm_data)
+  
+  calculate_all_bg_stats(sufficient_cgm_data)
+}
+
+############# FILTER TOO OLD, TOO RECENT, AND INSUFFICIENT DATA ################
+
+Mode <- function(x) {
+  # R doesn't have a base mode function...
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+calculate_minutes_per_record <- function(bg_df, 
+                                         n = 1000,
+                                         datetime_name = "datetime") {
+  # Estimates the amount of time between CGM readings for a given patient's
+  # CGM data. 
+  #
+  # Assumes that all CGM data for a particular patient will have one
+  # common time between all recordings (e.g., patient didn't switch CGMs)
+  
+  datetimes <- pull(bg_df, datetime_name)
+  
+  n_rows <- length(datetimes)
+  
+  # select n sequential rows at a random start point
+  if (n_rows > n) {
+     start <- sample(1:(n_rows - n + 1), 1)
+     datetimes <- datetimes[start:(start + n - 1)]
+  }
+  
+  datetimes %>%  
+    diff() %>% 
+    time_length(unit = "minutes") %>% 
+    abs() %>% 
+    round() %>% 
+    Mode()
+}
+  
+make_intervals <- function(bg_df, 
+                           datetime_name = "datetime") {
+  # Extracts date and hour from timestamps, and indicates 
+  # measurements that occur during nighttime. 
+  
+  bg_df %>% 
+    mutate(cgm_day = lubridate::date(.data[[datetime_name]]),
+           cgm_hour = lubridate::hour(.data[[datetime_name]]),
+           nighttime = cgm_hour < DAY_START)
+}
+
+filter_data_by_date <- function(bg_df, 
+                                start_date_chr = "1900-01-01",
+                                end_date_chr = "2100-12-31",
+                                datetime_name = "datetime") {
+  # Removes CGM data for invalid dates.
+  
+  start_date <- as.Date(start_date_chr)
+  end_date <- as.Date(end_date_chr)
+  
+  bg_df <-
+    filter(bg_df, between(.data[[datetime_name]], start_date, end_date))
+}
+                                
+filter_insufficient_data <- function(bg_df, cgm_day_name = "cgm_day") {
+  # Counts total possible CGM records for a given day and calculates the fraction
+  # of records actually recorded. Removes data from days with less than 
+  # MIN_DATA_REQUIRED fraction of records present.
+  
+  minutes_per_record <- calculate_minutes_per_record(bg_df)
+  
+  if (! minutes_per_record %in% MINUTES_BETWEEN_RECORDS) {
+    print(minutes_per_record)
+    abort("CGM data not recorded in 5, 10, 15, or 20 minute intervals.")
+  }
+  
+  cgm_records_per_hour <- 60 / minutes_per_record
+  
+  interval_df <- make_intervals(bg_df)
+  
+  exclude_df <-
+    interval_df %>%
+    group_by(cgm_day) %>%
+    summarize(n = n()) %>%
+    ungroup() %>%
+    mutate(prop = n / (cgm_records_per_hour * 24)) %>%
+    filter(prop < MIN_DATA_REQUIRED) %>%
+    select(cgm_day)
+  
+  anti_join(interval_df, exclude_df, by = c(cgm_day_name))
+}
+
+################################## SIMPLE BG STATS #############################
+
+calculate_cgm_days <- function(bg_df, cgm_day_name = "cgm_day") {
+  # Calculates the total number of days for which CGM data is available
+  
+  c(cgm_days = pull(bg_df, cgm_day_name) %>% 
+                unique() %>% 
+                length())
+}
+
+calculate_simple_bg_stat <- function(bg_df, f, stat_name) {
+  # bg_df: dataframe with CGM data, 
+  # f: function to perform on glucose values,
+  # stat_name: a string describing f
+  #
+  # Output: a named vector of the values calculated by applying f to glucose
+  # values during the full day, nighttime, and daytime.
+  
+  bg <- bg_df$glucose
+  nighttime <- bg_df$nighttime
+  bg_nighttime <- bg[nighttime]
+  bg_daytime <- bg[! nighttime]
+  
+  bg_stat <- c(f(bg), f(bg_nighttime), f(bg_daytime))
+  
+  names(bg_stat) <- c(str_c(stat_name, "_bg_full_day"),
+                      str_c(stat_name, "_bg_nighttime"),
+                      str_c(stat_name, "_bg_daytime"))
+  
+  bg_stat
+}
+
+################################## TIME IN RANGE ###############################
+
+make_bg_booleans <- function(bg_df) {
+  # Determines which range a particular CGM recording falls in.
+  
+  bg_df %>% 
+    mutate(very_low = glucose < VERY_LOW_BG_LIMIT,
+           low = between(glucose,
+                         VERY_LOW_BG_LIMIT,
+                         LOW_BG_LIMIT),
+           in_target_range = between(glucose, 
+                                     LOW_BG_LIMIT, 
+                                     HIGH_BG_LIMIT),
+           in_conservative_target_range = between(glucose, 
+                                                  LOW_BG_LIMIT, 
+                                                  CONSERVATIVE_HIGH_BG_LIMIT),
+           high = between(glucose,
+                          HIGH_BG_LIMIT,
+                          VERY_HIGH_BG_LIMIT),
+           very_high = glucose > VERY_HIGH_BG_LIMIT)
+}
+
+na_mean <- function(v) {
+  mean(v, na.rm = T)
+}
+
+na_sd <- function(v) {
+  sd(v, na.rm = T)
+}
+
+calculate_percent_in_given_range <- function(bg_df, range_name) {
+  # Calculates the percent of valid CGM readings that fall in the supplied
+  # range_name (e.g. "very_high") for each day interval
+  #
+  # Assumes that range_name is also a column in bg_df (created by make_bg_booleans)
+  
+  if (! range_name %in% colnames(bg_df)) {
+    abort(str_c(range_name, " column not contained in argument `bg_df` of ",
+                            "`calculate_percent_in_given_range`."))
+  }
+  
+  in_range <- pull(bg_df, range_name)
+  full_day_percent_in_range <- na_mean(in_range)
+  
+  nighttime <- bg_df$nighttime
+  in_range_nighttime <- in_range[nighttime]
+  in_range_daytime <- in_range[! nighttime]
+    
+  nighttime_percent_in_range <- na_mean(in_range_nighttime)
+  daytime_percent_in_range <- na_mean(in_range_daytime)
+  
+  percent_in_range_features <- 
+    c(full_day_percent_in_range,
+      nighttime_percent_in_range,
+      daytime_percent_in_range)
+  
+  names(percent_in_range_features) <- 
+    c(str_c(range_name, "_full_day"),
+      str_c(range_name, "_nighttime"),
+      str_c(range_name, "_daytime"))
+  
+  percent_in_range_features
+}
+
+calculate_stat_in_given_range <- function(bg_df, range_name, f, stat_name) {
+  # Applies f to valid BG readings that fall in the supplied
+  # range_name (e.g. "very_high") for each day interval
+  #
+  # Assumes that range_name is also a column in bg_df (created by make_bg_booleans)
+  
+  if (! range_name %in% colnames(bg_df)) {
+    abort(str_c(range_name, " column not contained in argument `bg_df` of ",
+                "`calculate_percent_in_given_range`."))
+  }
+  
+  if (! is_function(f)) {
+    abort(str_c("Argument `f` of `calculate_stat_in_given_range` is not a ",
+                "function"))
+  }
+  
+  in_range <- pull(bg_df, range_name)
+  nighttime <- pull(bg_df, "nighttime")
+  bg <- pull(bg_df, "glucose")
+  
+  in_range_nighttime <- in_range[nighttime]
+  in_range_daytime <- in_range[! nighttime]
+  
+  bg_in_range_full_day <- bg[in_range] 
+  bg_in_range_nighttime <- bg[in_range_nighttime]
+  bg_in_range_daytime <- bg[in_range_daytime]
+  
+  full_day_stat <- f(bg_in_range_full_day)
+  nighttime_stat <- f(bg_in_range_nighttime)
+  daytime_stat <- f(bg_in_range_daytime)
+  
+  stat_in_range_features <- 
+    c(full_day_stat,
+      nighttime_stat,
+      daytime_stat)
+  
+  names(stat_in_range_features) <- 
+    c(str_c(range_name, "_", stat_name, "_full_day"),
+      str_c(range_name, "_", stat_name, "_nighttime"),
+      str_c(range_name, "_", stat_name, "_daytime"))
+  
+  stat_in_range_features
+}
+
+calculate_all_bg_stats <- function(bg_df) {
+  
+  bg_df <- make_bg_booleans(bg_df)
+  
+  features <- c(calculate_cgm_days(bg_df),
+                calculate_simple_bg_stat(bg_df, na_mean, "mean"),
+                calculate_simple_bg_stat(bg_df, na_sd, "sd"))
+  
+  percent_in_range <- map(RANGE_NAMES, 
+                        ~ calculate_percent_in_given_range(bg_df, .))
+  
+  mean_in_range <- map(RANGE_NAMES, 
+                          ~ calculate_stat_in_given_range(bg_df, 
+                                                             ., 
+                                                             na_mean, 
+                                                             "mean"))
+  
+  sd_in_range <- map(RANGE_NAMES, 
+                       ~ calculate_stat_in_given_range(bg_df, 
+                                                          ., 
+                                                          na_sd, 
+                                                          "sd"))
+  
+   c(features, unlist(c(percent_in_range, mean_in_range, sd_in_range)))
+}
+
+
+
+################################## DEPRECATED ##################################
+
+# DEPRECATED: this code removed data by day/night interval
+#.
+# TODO: Make this more robust with DAY_START and DAY_END instead of using
+# midnight default as day end
+# 
+# daytime_record_multiplier <- ((24 - DAY_START) / DAY_START) - 1
+#
+# exclude_df <-
+#   interval_df %>%
+#   group_by(cgm_day, nighttime) %>%
+#   summarize(n = n()) %>%
+#   ungroup() %>%
+#   mutate(prop = n / (cgm_records_per_hour *
+#                       (DAY_START *
+#                         (1 + (! nighttime) * daytime_record_multiplier)))) %>%
+#   filter(prop < MIN_DATA_REQUIRED) %>%
+#   select(cgm_day, nighttime)
+# 
+# anti_join(interval_df, exclude_df, by = c("cgm_day", "nighttime"))
+
+
+################################## AUC #########################################
+
+# calculate_auc <- function(bg_df) {
+#   # Calculates the AUC for a given day of CGM data, and scales the AUC by the
+#   # number of valid recordings for that day
+#   
+#   time_diffs <- bg_df$datetime %>% 
+#                   diff() %>% 
+#                   time_length(unit = "minutes") %>% 
+#                   abs() %>% 
+#                   round()
+#   
+#   minutes_per_record <- calculate_minutes_per_record(bg_df)
+#   
+#   max_records_per_day <- 24 * calculate_records_per_hour(minutes_per_record)
+#   
+#   glucose <- bg_df$glucose
+#   
+#   # if time_diff is the same as normal recording interval, calculate auc, else 0
+#   aucs <- pmap(list(head(glucose, -1),
+#                     glucose[-1],
+#                     time_diffs),
+#        ~ if (..3 == minutes_per_record) {..3 * (..1 + ..2) / 2} else 0)
+#   
+#   aucs <- unlist(aucs)
+#   
+#   n_valid_aucs <- sum(aucs != 0)
+#   
+#   # if 288 possible recordings in a day, 287 possible aucs can be recorded
+#   sum(aucs) * ((max_records_per_day - 1)  / n_valid_aucs)
+# }
+# 
+# calculate_average_auc <- function(bg_df, range_name = NULL) {
+#   
+#   # if (! is.null(range_name)) {
+#   #   
+#   #   if (! range_name %in% colnames(bg_df)) {
+#   #     abort(str_c(range_name, " column not contained in argument `bg_df` of `calculate_average_auc`."))
+#   #   }
+#   #   
+#   #   in_range <- pull(bg_df, range_name)
+#   #   
+#   #   minutes_per_record <- calculate_minutes_per_record(bg_df)
+#   #   
+#   #   records_per_half_hour <- ceiling(60 / minutes_per_record)
+#   #   
+#   #   # collapse in_range 0 1 1 0 0 0 1 1 1 0 0 0 1 1 1 1 into 0 2 000 3 000 4
+#   #   # if the numbers are >= records_per_half_hour, expand into corresponding number of 1s
+#   #   # else expand into corresponding numbers of 0s
+#   #
+#   #   in_range_at_least_half_hour <- 
+#   #   
+#   #   bg_df <-
+#   #     filter(bg_df, in_range_at_least_half_hour)
+#   # }
+#   
+#   average_auc <- 
+#     bg_df %>% 
+#     group_by(cgm_day) %>% 
+#     group_map(~ calculate_auc(.x)) %>% 
+#     unlist() %>% 
+#     mean()
+#   
+#   names(average_auc) <- str_c("auc_", range_name)
+#   
+#   average_auc
+# }
